09188422a2585a4dea4eeefe3788885f0d5e2d89
asaei-telephone <abdullahsaei@hotmail.com>
2025-12-12 17:56:48 +0100

    add missing dependency

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC2164
 
-sudo apt-get install libopenblas-dev
- 
+sudo apt-get install libopenblas-dev sshpass
+
 python3 -m venv venv
 source venv/bin/activate
 bash install_depencies.sh
 pip3 install -r requirements.txt
+
+
+## First run ssh to pi_music once so it can store the ssh-key
+#sshpass -pPASSWORD_ESCAPE ssh user@pi_ip_address
